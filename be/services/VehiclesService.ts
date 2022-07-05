e335ae149272494d19e2304ef97dde4a84ee2a3c
Andrew Armbruster <AndrewADev@gmail.com>
2022-07-05 13:13:56 -0400

    feat: Allow bulk insert & retrieval of Vehicles
    Still using in-memory backing for our Vehicles service, but this will let us develop things like endpoints & FE until DB is working.

--- be/services/VehiclesService.ts
+++ be/services/VehiclesService.ts
@@ -1,21 +1,28 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-import {Vehicle} from '../entities/Vehicle';
+import { Vehicle } from "../entities/Vehicle";
 
 export class VehiclesService {
-    constructor() {
+  constructor() {}
 
-    }
+  // TODO: needs persistence
+  vehicles: Array<Vehicle> = [];
 
-    // TODO: needs persistence
-    vehicles: Array<Vehicle> = [];
+  createVehicle(entity: Vehicle) {
+    // TODO: replace with actual crud logic
+    entity.id = uuidv4();
 
-    createVehicle(entity: Vehicle) {
-        // TODO: replace with actual crud logic
-        entity.id = uuidv4();
-        
-        this.vehicles.push(entity);
+    this.vehicles.push(entity);
 
-        return entity;
-    }
+    return entity;
+  }
+
+  insertBulk(entities: Vehicle[]) {
+    this.vehicles = this.vehicles.concat(entities);
+    return entities.length;
+  }
+
+  getAll() {
+    return this.vehicles;
+  }
 }
