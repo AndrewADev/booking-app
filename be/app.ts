811198ea6af684f811a79834d50249bc7b81f495
Andrew Armbruster <AndrewADev@gmail.com>
2022-07-05 13:22:34 -0400

    feat: BE returns real Vehicles
    The main list endpoint for Vehicles now uses the VehiclesService to fetch the available vehicles.

--- be/app.ts
+++ be/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import { VehiclesService } from './services/VehiclesService';
 
 const app = express();
 
 const port = 3333;
 
+// TODO: better separation/setup
+
+const vehiclesService = new VehiclesService();
+
 app.get('/api/vehicles', (req, res) => {
-    res.send('Success!');
+    const vehicles = vehiclesService.getAll();
+
+    res.send(vehicles);
 });
 
 app.listen( port, () => {
